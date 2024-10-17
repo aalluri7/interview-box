4d92c36fee13adf9da79daed2dd10113137fe9ca
Aditya Varma Alluri <aalluri7@gmail.com>
2024-10-17 10:44:20 -0700

    wip

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './app';
+import { ContryRow } from './app'
 
 test('renders learn react link', () => {
   render(<App />);
-  const headerElement = screen.getByText(/hello world/i);
+  const headerElement = screen.getByText(/search/i);
   expect(headerElement).toBeInTheDocument();
 });
+
+const data = { "name": { "common": "Tokelau", "official": "Tokelau", "nativeName": { "eng": { "official": "Tokelau", "common": "Tokelau" }, "smo": { "official": "Tokelau", "common": "Tokelau" }, "tkl": { "official": "Tokelau", "common": "Tokelau" } } }, "currencies": { "NZD": { "name": "New Zealand dollar", "symbol": "$" } }, "capital": ["Fakaofo"], "region": "Oceania", "languages": { "eng": "English", "smo": "Samoan", "tkl": "Tokelauan" }, "borders": [], "flag": "🇹🇰", "population": 1411 };
+test('renders contry info correctly', () => {
+  render(<ContryRow c={data} />)
+  const nameElement = screen.getByText(/Tokelau/i)
+})
+
+
+test('renders more info on btn click', () => {
+  render(<ContryRow c={data} />);
+  const btn = screen.getByText(/View More/i);
+  if (btn) {
+    fireEvent.click(btn);
+  }
+  const CurrenciesElm = screen.getByText(/Currencies/i)
+  expect(CurrenciesElm).toBeInTheDocument();
+})
--- src/app.tsx
+++ src/app.tsx
@@ -7,13 +7,122 @@ import {
 } from "react-router-dom";
 import ErrorPage from "./error-page"
 
-const HelloWorld = () => {
-  return <header className={css.title}>Hello world!</header>
+/*
+Each country should have
+- Its name
+- Its flag (display the icon in smaller size)
+- Population
+- Region
+- Capital
+- "View More" button to display the additional details of the country
+
+## A widget to display the additional details of the country as below
+- Its flag (should display larger image while viewing the details)
+- Population
+- Region
+- Capital
+- Native Name
+- Currencies used
+- Languages
+- Border countries
+
+*/
+type ContryType = {
+  borders: string[],
+  languages: Record<string, string>,
+  currencies: Record<string,
+    { name: string, symbol: string }>,
+  name: { common: string, official: string, nativeName: Record<string, { common: string, official: string }> },
+  flag: string, population: number, region: string, capital: string[]
+};
+
+export const ContryRow = ({ c }: { c: ContryType }) => {
+
+  const [more, setMore] = React.useState(false);
+
+  const onClick = React.useCallback(() => {
+    setMore(!more);
+  }, [more])
+
+  let currencies = ''
+  if (c.currencies) {
+    currencies = Object.keys(c.currencies)[0];
+  }
+  let languages = ''
+  let langs = [];
+  if (c.languages) {
+    langs = Object.keys(c.languages);
+    languages = langs.join(',');
+  }
+  let nativeName = ''
+  if (c.name?.nativeName) {
+    const nativeKey = Object.keys(c.name.nativeName)[0];
+    if (nativeKey) {
+      nativeName = c.name?.nativeName[nativeKey].common;
+
+    }
+  }
+  let borders = ''
+  if (c.borders) {
+    borders = c.borders.join(",")
+  }
+
+  return <div className={css.row}>
+    <div className={css.info}>
+      <div className={more ? css.flag : ''}>{c.flag}</div>
+      <div>name: {c.name.common}</div>
+      <div>Population: {c.population}</div>
+      <div>Region: {c.region}</div>
+      <div>Capital: {c.capital?.join(',')}</div>
+      {more && (<>
+        <div>Native Name:{nativeName}</div>
+        <div>Currencies: {currencies}</div>
+        <div>Languages: {languages}</div>
+        <div>borders: {borders}</div>
+      </>)
+      }
+    </div>
+    <button onClick={onClick} className={css.btn}> {more ? 'collapse' : 'View More'}</button>
+
+  </div>;
+}
+
+const ContriesList = () => {
+  const [contires, setData] = React.useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [filtres, setFiltred] = React.useState<ContryType[]>([]);
+  React.useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=borders,region,languages,currencies,name,flag,population,capital').then(response => {
+      response.json().then(data => {
+        setData(data);
+        setFiltred(data);
+      });
+    })
+  }, [])
+
+  React.useEffect(() => {
+    const filtred = contires.filter((c: ContryType) => {
+      if (c.name.common.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || searchTerm === '') {
+        return true
+      }
+    })
+    setFiltred(filtred);
+  }, [searchTerm])
+
+  const onChange = React.useCallback((e: any) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+  }, [searchTerm])
+  return <div className={css.body}>
+    Search Contry Name: <input onChange={onChange} value={searchTerm}></input>
+    {filtres.map(c => {
+      return <ContryRow key={c.name.common} c={c} />
+    })}</div>
 }
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HelloWorld />,
+    element: <ContriesList />,
     errorElement: <ErrorPage />,
   },
 ]);
